5cb991be293ff9bb867622d932dd842f56059769
[Harshil Karir] <[harshilkarir72@gmail.com]>
2024-02-21 03:25:50 +0530

    more changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import vitePluginRequire from "vite-plugin-require";
+import commonjs from "rollup-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginRequire.default()],
+  plugins: [react(), vitePluginRequire.default(), commonjs()],
 });
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // other webpack configuration...
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
