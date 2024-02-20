ffffaf6dd4297ae60a03b86cf57a33fe6105bcf8
[Harshil Karir] <[harshilkarir72@gmail.com]>
2024-02-20 20:03:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="6105ce1a-ea6f-4e8d-978e-23d7e8b75ed3"
+      userName="hrshil72"
+      userSecret="harshil123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
