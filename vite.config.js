268aa8fee975cc218b584ce51dcbe38f8ecd9d5f
[Harshil Karir] <[harshilkarir72@gmail.com]>
2024-02-21 03:41:08 +0530

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import vitePluginRequire from "vite-plugin-require";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginRequire.default(), commonjs()],
+  plugins: [react(), commonjs()],
 });
